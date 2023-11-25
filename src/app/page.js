9c9290dc00d21883e943f0f128ffba165aece006
Jignesh <jignesh.sharma.707@gmail.com>
2023-11-25 09:19:35 +0530

    vers

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import ParthVideo from '@/components/Parth.mp4';
+import ParthVideo from '@/../public/video/Parth.mp4';
 
 export default function Home() {
   return (
     <>
       <section className="border mt-[60px] h-[calc(100vh-60px)] bg-red-100">
         <div className="">
-          <video controls width="100%" height="100%">
+          <video controls autoPlay muted width="100%" height="100%">
             <source src={ParthVideo} type="video/mp4" />
           </video>
         </div>
