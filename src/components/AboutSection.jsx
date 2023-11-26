8a37cd1bf3087e281114d1a09bae30e39791a2df
Jignesh <jignesh.sharma.707@gmail.com>
2023-11-26 22:42:53 +0530

    about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -6,9 +6,11 @@ import { IoIosArrowDropright } from 'react-icons/io';
 const AboutSection = () => {
   return (
     <div className="py-10">
-      <div className="flex flex-col justify-center items-center text-center">
-        <h1 className="text-5xl font-bold text-[#C4956A]">Legacy Of Raheja</h1>
-        <p className="mt-10 w-[100%] max-w-[1000px] leading-7">
+      <div className="flex flex-col justify-center items-center text-start md:text-center px-5">
+        <h1 className="text-3xl md:text-5xl font-bold text-[#C4956A]">
+          Legacy Of Raheja
+        </h1>
+        <p className="mt-4 md:mt-10 w-[100%] max-w-[1000px] leading-7 px-0 md:px-10 xl:px-0">
           Raheja Developers is one of the largest Real Estate companies in India
           established in the year 1990 by Mr. Navin M. Raheja. The company has
           always maintained path breaking status and has pioneered various
@@ -21,18 +23,18 @@ const AboutSection = () => {
 
       <div className="flex flex-row justify-center items-center gap-5 py-4 mt-10">
         <Link href={'/about'}>
-          <button className="px-6 py-3">
+          <button className="px-6 py-3 border border-black/5">
             Read More <br />
             <span>
               <IoIosArrowDropright
-                className="mx-auto text-black/60"
+                className="mx-auto text-black/80"
                 size={28}
               />
             </span>
           </button>
         </Link>
         <Link href={'#'}>
-          <button className="px-6 py-3">
+          <button className="px-6 py-3 border border-black/5">
             View More <br />
             <span>
               <FaYoutube className="mx-auto" size={28} color="red" />
